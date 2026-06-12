f1edb52f1a142debf3ffb7a693b8d1ac7a182998
Fionn Smyth <smythf6@tcd.ie>
2026-06-12 11:19:41 +0100

    updated ALU testbench

--- src/testbenches/ALU_tb.sv
+++ src/testbenches/ALU_tb.sv
@@ -2,4 +2,31 @@
 
 module ALU_tb;
 
+    reg[7:0] a_test, b_test, c_test;
+    reg[2:0] fxn_test;
+      
+    ALU uut (.a(a_test), .b(b_test), .fxn(fxn_test), .c(c_test));
+    
+    
+    initial begin
+        for (int i = 0; i < 8; i++) begin
+            fxn_test <= i;
+            #(100);
+        end    
+    end
+    
+    always @ (fxn_test) begin
+        a_test = 8'h4;
+        b_test = 8'h3;
+        #(25);
+        a_test = 8'h5;
+        b_test = 8'h5;
+        #(25);
+        a_test = 8'h0;
+        b_test = 8'h1;
+        #(25);
+        a_test = 8'hFF;
+        b_test = 8'h20;
+    end
+    
 endmodule
